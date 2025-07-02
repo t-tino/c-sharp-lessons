af7c9520b29c3c17dca218f3da54d3bd1744a0d6
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-07-02 19:57:36 +0300

    While Loops

---
+++ Sources/While-Loops/Program.cs
@@ -0,0 +1,48 @@
+﻿class WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a number (0 to exit):");
+                int number;
+                while (true)
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                    if (number == 0)
+                    {
+                        Console.WriteLine("Exiting the loop.");
+                        break;
+                    }
+                    Console.WriteLine($"You entered: {number}");
+                }
+
+                do
+                {
+                    Console.WriteLine("Please enter another number (0 to exit):");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    if (number == 0)
+                    {
+                        Console.WriteLine("Exiting the do-while loop.");
+                        break;
+                    }
+                    Console.WriteLine($"You entered: {number}");
+                } while (true);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Thank you for using the number input program.");
+            }
+        }
+    }
+}
