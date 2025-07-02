1f2666d03594887e28113c80adcc4b1a435f1a0b
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-07-02 20:06:05 +0300

    Methods

---
+++ Sources/Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿class Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a number to calculate its square:");
+                int number = Convert.ToInt32(Console.ReadLine());
+                int result = CalculateSquare(number);
+                Console.WriteLine($"The square of {number} is {result}.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Thank you for using the square calculation program.");
+            }
+        }
+
+        static int CalculateSquare(int num)
+        {
+            return num * num;
+        }
+    }
+}
