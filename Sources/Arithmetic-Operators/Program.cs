f5222c1f9d6435a1bac93a25b3bff60a3b0615ec
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-07-02 18:14:37 +0300

    Arithmetic-Operators

---
+++ Sources/Arithmetic-Operators/Program.cs
@@ -0,0 +1,31 @@
+﻿class ArithmeticOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int friends = 5;
+
+            // friends = friends + 1; // Increment
+            // friends += 1; // Increment
+            // friends++; // Increment
+
+            // friends = friends - 1; // Decrement
+            // friends -= 1; // Decrement
+            // friends--; // Decrement
+
+            // friends = friends * 2; // Multiply
+            // friends *= 2; // Multiply
+
+            // friends = friends / 2; // Divide
+            // friends /= 2; // Divide
+
+            // friends = friends % 2; // Modulus
+            friends %= 2; // Modulus
+
+            Console.WriteLine(friends);
+            Console.WriteLine("You dont have friends!");
+            Console.WriteLine("Maybe you have a cat?");
+        }
+    }
+}
