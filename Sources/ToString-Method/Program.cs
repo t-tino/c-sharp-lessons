442d8402c6276d0815d0b169f1d404663f74e7a0
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-07-02 20:46:17 +0300

    ToString Method

---
+++ Sources/ToString-Method/Program.cs
@@ -0,0 +1,30 @@
+﻿class ToStringMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("John", "Doe", 30);
+            Console.WriteLine(person.ToString());
+        }
+
+        class Person
+        {
+            public string FirstName { get; }
+            public string LastName { get; }
+            public int Age { get; }
+
+            public Person(string firstName, string lastName, int age)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                Age = age;
+            }
+
+            public override string ToString()
+            {
+                return $"{FirstName} {LastName}, Age: {Age}";
+            }
+        }
+    }
+}
